df36abeb3aebf2c2ec5f7b27a5e6cddf8d436547
Hole 30601 <121752219+Hole30601@users.noreply.github.com>
2026-05-21 06:29:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
     BOT_TOKEN: "1473026517432223818:zabyBKIlgCydSLgQHMjXlfhAJiokBykgtmsdlLYjrKZLDltsDIRcpJzrYDjksoJL",
 
-    URL:
-    "https://xemlichcatdien.com/lich-cat-dien-gia-binh-bac-ninh/"
+    URL: "https://xemlichcatdien.com/lich-cat-dien-gia-binh-bac-ninh/",
+
+    USER_ID: "3111c131df6d36336f7c" // ID Zalo cần nhận thông báo
 };
