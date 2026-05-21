194743d8e09fc3922ac06562634b66faa0ed9e68
Hole 30601 <121752219+Hole30601@users.noreply.github.com>
2026-05-21 21:10:56 +0700

    Update getAllUsers.js

--- getAllUsers.js
+++ getAllUsers.js
@@ -1,16 +1,18 @@
 const db = require("./firebase");
 
 async function getAllUsers() {
-    const snapshot =
-        await db.ref("users").get();
+    const snapshot = await db.ref("users").get();
 
     const data = snapshot.val();
 
     if (!data) {
+        console.log("Không có user");
         return [];
     }
 
-    return Object.keys(data);
+    return Object.values(data)
+        .map(user => user.chatId)
+        .filter(Boolean);
 }
 
 module.exports = getAllUsers;
