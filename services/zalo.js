bfe99e53658919abdfeb6751fb7df59d35a2c9fd
Hole 30601 <121752219+Hole30601@users.noreply.github.com>
2026-05-21 11:43:08 +0700

    Update zalo.js

--- services/zalo.js
+++ services/zalo.js
@@ -4,9 +4,9 @@ const { BOT_TOKEN, USER_ID } = require("../config");
 async function sendMessage(text, userId = USER_ID) {
     try {
         const res = await axios.post(
-            "https://bot.zapps.me/api/sendMessage",
+            "https://bot-api.zaloplatforms.com/bot${BOT_TOKEN}/sendMessage",
             {
-                botToken: BOT_TOKEN,
+                
                 userId: userId,
                 message: text
             }
