f4d16aae4be6cb0dfc7eaa013f4111eba3132851
Hole 30601 <121752219+Hole30601@users.noreply.github.com>
2026-05-21 11:11:13 +0700

    Create setWebhook.js

---
+++ setWebhook.js
@@ -0,0 +1,35 @@
+const axios = require("axios");
+
+const BOT_TOKEN = process.env.BOT_TOKEN;
+
+async function setWebhook() {
+
+    try {
+
+        const webhookUrl =
+            "https://ten-app.onrender.com/webhook";
+
+        const response =
+            await axios.post(
+                `https://bot-api.zaloplatforms.com/bot${BOT_TOKEN}/setWebhook`,
+                {
+                    url: webhookUrl,
+                    secret_token: "my-secret-key"
+                }
+            );
+
+        console.log(
+            "Webhook OK:",
+            response.data
+        );
+
+    } catch (err) {
+
+        console.error(
+            "Webhook Error:",
+            err.response?.data || err.message
+        );
+    }
+}
+
+module.exports = setWebhook;
